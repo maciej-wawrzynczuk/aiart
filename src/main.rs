54b35b2c08229a01562824e3ed044e51736e080c
Maciej.Wawrzynczuk <maciej.wawrzynczuk@capgemini.com>
2026-04-17 16:22:07 +0200

    random line...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use rand::random_range;
+
+#[derive(Debug)]
+struct SimpleLine {
+    x0: f32,
+    y0: f32,
+    x1: f32,
+    y1: f32
 }
+
+fn main() {
+    let l = SimpleLine::new_random();
+    println!("{l:?}")
+}
+
+impl SimpleLine {
+    fn new_random() -> Self {
+        Self {
+            x0: random_range(0.0..1.0),
+            y0: random_range(0.0..1.0),
+            x1: random_range(0.0..1.0),
+            y1: random_range(0.0..1.0),
+        }
+    } 
+}
+

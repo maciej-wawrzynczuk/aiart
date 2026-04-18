17749cbec281f50251986da7edb774b2695bea96
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-04-18 12:52:59 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,10 @@
 mod floats;
-use core::fmt;
+mod svg;
 
-use anyhow::{Context, Result};
-use num_traits::{Float, NumCast};
-
+use anyhow::Result;
 use crate::floats::Line;
+use crate::svg::{CanvasSize, SVGLine};
 
-struct SVGLine {
-    x1: u16,
-    x2: u16,
-    y1: u16,
-    y2: u16,
-}
-
-struct CanvasSize {
-    x: u16,
-    y: u16,
-}
-
-impl SVGLine {
-    fn from_line<T: Float>(l: Line<T>, c: CanvasSize) -> Result<Self> {
-        Ok(Self {
-            x1: f2canvas(l.start.x, c.x)?,
-            y1: f2canvas(l.start.y, c.y)?,
-            x2: f2canvas(l.end.x, c.x)?,
-            y2: f2canvas(l.end.y, c.y)?,
-        })
-    }
-}
-
-impl fmt::Display for SVGLine {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"currentColor\" stroke-width=\"2\"/>",
-            self.x1, self.y1, self.x2, self.y2
-        )
-    }
-}
-
-fn f2canvas<F: Float, I: NumCast>(f: F, i: I) -> Result<I> {
-    let i_as_f: F = NumCast::from(i).context("Cast error")?;
-    NumCast::from((f * i_as_f).round()).context("Cast error")
-}
 
 fn main() -> Result<()> {
     let l: Line<f32> = Line::new_random();
---
+++ src/svg.rs
@@ -0,0 +1,44 @@
+use core::fmt;
+
+use anyhow::{Context, Result};
+use num_traits::{Float, NumCast};
+
+use crate::floats::Line;
+
+pub struct SVGLine {
+    x1: u16,
+    x2: u16,
+    y1: u16,
+    y2: u16,
+}
+
+pub struct CanvasSize {
+    pub x: u16,
+    pub y: u16,
+}
+
+impl SVGLine {
+    pub fn from_line<T: Float>(l: Line<T>, c: CanvasSize) -> Result<Self> {
+        Ok(Self {
+            x1: f2canvas(l.start.x, c.x)?,
+            y1: f2canvas(l.start.y, c.y)?,
+            x2: f2canvas(l.end.x, c.x)?,
+            y2: f2canvas(l.end.y, c.y)?,
+        })
+    }
+}
+
+impl fmt::Display for SVGLine {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"currentColor\" stroke-width=\"2\"/>",
+            self.x1, self.y1, self.x2, self.y2
+        )
+    }
+}
+
+fn f2canvas<F: Float, I: NumCast>(f: F, i: I) -> Result<I> {
+    let i_as_f: F = NumCast::from(i).context("Cast error")?;
+    NumCast::from((f * i_as_f).round()).context("Cast error")
+}

1b8ef5acbc3a8a783817e6ccfa55ebef71077d26
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-04-18 14:09:37 +0200

    print test

--- src/svg.rs
+++ src/svg.rs
@@ -18,6 +18,9 @@ pub struct CanvasSize {
 }
 
 impl SVGLine {
+    pub fn new(x1: u16, x2: u16, y1: u16, y2: u16) -> Self {
+        Self {x1, x2, y1, y2}
+    }
     pub fn from_line<T: Float>(l: Line<T>, c: CanvasSize) -> Result<Self> {
         Ok(Self {
             x1: f2canvas(l.start.x, c.x)?,
@@ -32,7 +35,7 @@ impl fmt::Display for SVGLine {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"currentColor\" stroke-width=\"2\"/>",
+            "<line x1=\"{}\" y1=\"{}\" x2=\"{}\" y2=\"{}\" stroke=\"black\" stroke-width=\"2\"/>",
             self.x1, self.y1, self.x2, self.y2
         )
     }
@@ -46,7 +49,7 @@ fn f2canvas<F: Float, I: NumCast>(f: F, i: I) -> Result<I> {
 
 #[cfg(test)]
 mod test{
-    use crate::svg::f2canvas;
+    use crate::svg::{SVGLine, f2canvas};
 
     #[test]
     fn f2c_boundaries() {
@@ -57,5 +60,14 @@ mod test{
     #[test]
     fn f2c_mid() {
         assert_eq!(f2canvas(0.5, 100).unwrap(), 50);
+    }
+
+    #[test]
+    fn display_line() {
+        let l = SVGLine::new(0, 1, 2, 3);
+        let expected = "<line x1=\"0\" y1=\"2\" x2=\"1\" y2=\"3\" stroke=\"black\" stroke-width=\"2\"/>"
+            .to_string();
+        let result = l.to_string();
+        assert_eq!(result, expected);
     }
 }

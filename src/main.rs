49af69fd10d233bc1bc1f19fa8a201e33798de4f
Maciej.Wawrzynczuk <maciej.wawrzynczuk@capgemini.com>
2026-04-29 10:02:03 +0200

    webservice shows image

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,24 @@ mod perceptron;
 mod svg;
 
 use crate::svg::Svg;
-use anyhow::Result;
+use axum::{Router, http::header, response::IntoResponse, routing::get};
 use rand::random_range;
+use tokio::net::TcpListener;
 
-fn main() -> Result<()> {
-    let s: Vec<f32> = (0..100).map(|_| random_range(0.0..1.0)).collect();
-    let s = Svg::new4(640, 480, &s)?;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new()
+        .route("/ping", get(|| async { "Pong" }))
+        .route("/img1.svg", get(get_img1));
+    let listener = TcpListener::bind("0.0.0.0:30001").await?;
+    axum::serve(listener, app).await?;
 
-    println!("{s}");
     Ok(())
+}
+
+async fn get_img1() -> impl IntoResponse {
+    let s: Vec<f32> = (0..100).map(|_| random_range(0.0..1.0)).collect();
+    let svg = Svg::new4(640, 480, &s).unwrap().to_string();
+
+    ([(header::CONTENT_TYPE, "image/svg+xml")], svg)
 }
